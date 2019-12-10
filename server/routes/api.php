c5dda56e69f917d2bbb91b8c3c250fcf017c1f1b
“e <2083184354@qq.com>
2019-12-10 16:07:59 +0800

    api

--- server/routes/api.php
+++ server/routes/api.php
@@ -16,3 +16,13 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('common')->namespace('Common')->group(function ($router) {
+    $router->post('login', 'UserController@login');
+    $router->post('logout', 'UserController@logout');
+});
+
+
+Route::prefix('food')->middleware([])->namespace('Food')->group(function ($router) {
+    $router->get('food', 'FoodController@index');
+});
