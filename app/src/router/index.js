5cea773c25d806a841473e6947a19bca995d6e83
“e <2083184354@qq.com>
2019-12-12 15:43:52 +0800

    app路由上传

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -11,11 +11,6 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    },
-    {
-      path: '/index',
-      name: 'index',
-      component: Index
     }
   ]
 })
