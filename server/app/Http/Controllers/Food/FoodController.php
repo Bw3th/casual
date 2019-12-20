35f94fd055a95e6094d9969fddfa4fc7630f6601
“e <2083184354@qq.com>
2019-12-20 16:35:41 +0800

    图片查看

--- server/app/Http/Controllers/Food/FoodController.php
+++ server/app/Http/Controllers/Food/FoodController.php
@@ -5,12 +5,37 @@ namespace App\Http\Controllers\Food;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Foods;
+use App\File;
 
 class FoodController extends Controller
 {
     public function index()
     {
         $res = Foods::where('is_delete',0)->paginate();
+        foreach($res as $k => $v){
+            $file = explode(',',$v['file']);
+            $img = [];
+            $imgnum = 0;
+            $files = [];
+            $filenum = 0;
+            if($file[0]){
+                foreach($file as $key => $val){
+                    $path = File::find($val);
+                    
+                    if(in_array($path['ext'],['png','jpg','jpeg'])){
+                        $img[$imgnum]['url'] = "http://122.51.233.226/Uploads".$path['savepath'].'/'.$path['savename'].'.'.$path['ext'];
+                        $img[$imgnum]['filename'] =$path['name'].'.'.$path['ext'];
+                        $imgnum += 1;
+                    }else{
+                        $files[$filenum]['url'] = "http://122.51.233.226/Uploads".$path['savepath'].'/'.$path['savename'].'.'.$path['ext'];
+                        $files[$filenum]['filename'] =$path['name'].'.'.$path['ext'];
+                        $filenum += 1;
+                    }
+                }
+            }
+            $res[$k]['file'] = $img;
+            $res[$k]['otherFile'] = $files;
+        }
         return responder()->success($res);
     }
 
